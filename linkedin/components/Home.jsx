7064d38263580b09580f9d3c1466ceab774ce030
Shanmugasundaram Raja <100921063+ShanmugasundaramRaja@users.noreply.github.com>
2022-07-12 12:48:30 +0200

    Update Home.jsx

--- linkedin/components/Home.jsx
+++ linkedin/components/Home.jsx
@@ -4,26 +4,68 @@ import RightSideBar from "./RightSideBar"
 
 
 const Home = () => {
-  
-  
+    const [posts, setPosts] = useState([]);
+    const [ profile , setProfile ] = useState([])
+    const fetchPosts = async () => {
+        try {
+          let response = await fetch(
+          
 
-  const [ profile , setProfile ] = useState([])
+
+
+          );
+          if (response.ok) {
+             
+
+
+
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      const fetchProfile = async () => {
+        try {
+          let response = await fetch(
+           
+
+
+
+          );
+          if (response.ok) {
+           
+
+
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      useEffect(() => {
+        fetchPosts();
+        fetchProfile();
+      }, []);
 
   
 
   return (
     <Container style={{ marginTop: "100px" }}>
-      <Row>
-        <Col xs={12} md={4} lg={2} style={{ height: "100%" }}>
-          <LeftSideBarCardProfile profile={profile}/>
-        </Col>
-       
+    <Row>
+      <Col xs={12} md={4} lg={2} style={{ height: "100%" }}>
+        <LeftSideBarCardProfile profile={profile}/>
+      </Col>
+      <Col xs={12} md={8} lg={6} style={{ height: "100%" }}>
+        <NewPost profile={profile} setPosts={setPosts} posts={posts} fetchPosts={fetchPosts} />
+        <ExistingPosts profile={profile} posts={posts} fetchPosts={fetchPosts} />
+      </Col>
 
-        <Col md={4} className="d-none d-md-block" style={{ height: "100%" }}>
-          <RightSideBar />
-        </Col>
-      </Row>
-    </Container>
+      <Col md={4} className="d-none d-md-block" style={{ height: "100%" }}>
+        <RightSideBar />
+      </Col>
+    </Row>
+  </Container>
   );
 };
 
